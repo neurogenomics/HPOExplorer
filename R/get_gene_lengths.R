38851d12fabaac7d4f8f5d32fc7d6ba8d55cabfe
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2023-01-24 11:15:21 +0000

    Allow different refs in get_gene_lengths

--- R/get_gene_lengths.R
+++ R/get_gene_lengths.R
@@ -1,8 +1,8 @@
 get_gene_lengths <- function(gene_list,
                              keep_seqnames = c(seq_len(22),"X","Y"),
+                             ensembl_version = 75,
                              verbose = TRUE){
 
-  requireNamespace("EnsDb.Hsapiens.v75")
   requireNamespace("ensembldb")
   requireNamespace("AnnotationFilter")
   messager("Gathering gene metadata",v=verbose)
@@ -11,12 +11,24 @@ get_gene_lengths <- function(gene_list,
       paste0("chr",keep_seqnames),
       keep_seqnames)
   )
-  txdb <- EnsDb.Hsapiens.v75::EnsDb.Hsapiens.v75
-  tx_gr <- ensembldb::genes(txdb,
-                            columns = c(ensembldb::listColumns(txdb, "gene"), "entrezid"),
-                            filter=AnnotationFilter::AnnotationFilterList(
-                              AnnotationFilter::SymbolFilter(value = unique(gene_list)),
-                              AnnotationFilter::SeqNameFilter(value = keep_seqnames)
-                            ))
+  #### Get gene reference database ####
+  if(ensembl_version==75){
+    #### Outdated ref ####
+    requireNamespace("EnsDb.Hsapiens.v75")
+    txdb <- EnsDb.Hsapiens.v75::EnsDb.Hsapiens.v75
+  } else {
+    #### Updated ref ####
+    requireNamespace("AnnotationHub")
+    hub <- AnnotationHub::AnnotationHub()
+    q <- AnnotationHub::query(hub, c("EnsDb",ensembl_version,"sapiens"))
+    txdb <- q[[length(q)]]
+  }
+  tx_gr <- ensembldb::genes(
+    txdb,
+    columns = c(ensembldb::listColumns(txdb, "gene"), "entrezid"),
+    filter=AnnotationFilter::AnnotationFilterList(
+      AnnotationFilter::SymbolFilter(value = unique(gene_list)),
+      AnnotationFilter::SeqNameFilter(value = keep_seqnames)
+    ))
   return(tx_gr)
 }
